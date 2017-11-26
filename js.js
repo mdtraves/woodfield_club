5ff8f104a30f6fe273041e09dfcf16fa0d654f88
mike traves <mdtraves@hotmail.com>
2017-11-26 21:38:19 +0000

    removed some uneccessary images and removed instances of prepos, using grunt in future

--- js.js
+++ js.js
@@ -1,6 +1,28 @@
+/*global $ */
+
+
 $(document).ready(function(){
     $(".title-h1").hide();
-    $(".title-h1").slideDown(1000);
-     $(".navbar").hide();
-    $(".navbar").show(900);
-});
+    $(".title-h1").slideDown(400);
+    $(".navbar").hide();
+    $(".navbar").slideDown(300);
+    
+    $(".img-responsive").click(function(){
+        $(this).fadeOut(300);
+        $(this).slideDown(700);
+        
+    });
+    });
+
+     
+    function initMap() {
+        var uluru = {lat: 53.502059, lng: -1.145217};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 10,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
